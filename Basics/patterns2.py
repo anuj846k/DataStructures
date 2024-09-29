7d1d932e202acfcccbf12402650338d1fbeb17c8
anuj846k <anuj846k@gmail.com>
2024-09-29 12:36:22 +0530

    Pattern2 more questions

--- Basics/patterns2.py
+++ Basics/patterns2.py
@@ -62,13 +62,6 @@ n= int(input("Enter the number :"))
 
 
 
-# for i in range(1,n+1):
-#     for ch in range(ord('A'),ord('A')-i,):
-#         print(chr(ch),end=" ")
-#     print()
-
-
-
 
 
 # Pattern 15 output
@@ -80,7 +73,97 @@ n= int(input("Enter the number :"))
 # A B
 # A
 
-for i in range(1,n+1):
-    for ch in range(ord('A'),ord('A')+(n-i)+1):
-        print(chr(ch),end=" ")
-    print()
+# for i in range(1,n+1):
+#     for ch in range(ord('A'),ord('A')+(n-i)+1):
+#         print(chr(ch),end=" ")
+#     print()
+
+
+
+# Pattern 16 output
+
+# A 
+# B B
+# C C C
+# D D D D
+# E E E E E
+# F F F F F F
+
+# for i in range(1,n+1):
+#     ch = ord('A')+i-1
+#     for j in range(i):
+#         print(chr(ch),end=" ")
+#     print()
+
+
+
+# Pattern 17 output
+
+#      A     
+#     ABA    
+#    ABCBA   
+#   ABCDCBA  
+#  ABCDEDCBA 
+# ABCDEFEDCBA
+
+
+# for i in range(0,n):
+#     for j in range(0,n-i-1):
+#         print(" ",end=" ")
+
+#     ch = ord('A')
+#     for j in range(0,i+1):
+#         print(chr(ch + j),end=" ")
+    
+#     for j in range(i-1,-1,-1):
+#         print(chr(ch + j),end=" ")
+
+#     print()
+    
+
+
+# pattern 18 output
+# F
+# E F
+# D E F
+# C D E F
+# B C D E F
+# A B C D E 
+
+
+# for i in range(n):
+#     ch = ord('F')-i
+#     for j in range(i+1):
+#         print(chr(ch +j),end=" ");
+#     print()
+
+
+
+# Pattern 19 output
+
+# ******
+# **  **
+# *    *
+# *    *
+# **  **
+# ******
+
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print("*",end=" ")
+#     for j in range(0,2*(i)):
+#         print(" ",end=" ")
+
+#     for j in range(0,n-i):
+#         print("*",end=" ")
+#     print()
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print("*",end=" ")
+#     for j in range(0,2*(n-i-1)):
+#         print(" ",end=" ")
+#     for j in range(0,i+1):
+#         print("*",end=" ")
+#     print()
+
+
